dbc019e5c05848b70ebc7e8449d767f5ce2d9968
dragoplut <oleksandr_p@i.ua>
2015-07-02 15:35:50 +0300

    create signin.php unable check code err 500 on signin.php

--- blocks/leftmenu.php
+++ blocks/leftmenu.php
@@ -1,11 +1,13 @@
+<!-- left menu -->
 <div class="container-fluid">
   <div class="row">
     <div class="col-sm-3 col-md-2 sidebar">
       <ul class="nav nav-sidebar">
         <li class="active"><a href="/index.php">Dashboard<span class="sr-only">(current)</span></a></li>
         <li><a href="clients.php">Clients</a></li>
-        <li><a href="signin.html">Sign in</a></li>       
+        <li><a href="signin.php">Sign in</a></li>       
       </ul>
+      <!-- keep ul for possible use -->
       <ul class="nav nav-sidebar">
         <li><a href=""></a></li>            
       </ul>          
