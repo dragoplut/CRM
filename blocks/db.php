1637b8644bd09932ccdae6ea8b1d7fb993ffa3d9
dragoplut <oleksandr_p@i.ua>
2015-07-04 20:19:41 +0300

    mysqli_connect truble in db.php

--- blocks/db.php
+++ blocks/db.php
@@ -1,10 +1,9 @@
 <?php
-	$conn = mysql_connect('Localhost: /db/clients.db','root','1076891') or die ('Unable to connect with MySQl.');
-	$db = mysql_select_db('clients', $conn) or die ('Unable to connect with database');
-	$sql = mysql_query("SELECT 'id','firs_name','last_name','age' FROM 'clientsinfo' ORDER BY 'id' DESC",$conn);
+	$mysqli = mysqli_connect('localhost','root','1076891','db/clients.db') or die ('Unable to connect with MySQl.');
+	$result = $mysqli->query("SELECT 'id','firs_name','last_name','age' FROM 'clientsinfo' ORDER BY 'id' DESC");
 	echo ("<table border = '1'>");
 	echo ("<tr><td>ID</td><td>Firs name</td><td>Last Name</td><td>Age</td></tr>");
-	while ($tablerows = mysql_fetch_row($sql)) 
+	while ($tablerows = $result->fetch_assoc()) 
 	{
 		echo ("
 				<tr>
@@ -16,5 +15,6 @@
 			");
 	}
 	echo ("</table>");
-	mysql_close($conn);
+	$result->close();
+	$mysqli->close();
 ?>
