d687c3f15a60f6ccd007706072dd284939896af7
dragoplut <oleksandr_p@i.ua>
2015-07-04 12:09:36 +0300

    signin.php changes

--- signin.php
+++ signin.php
@@ -7,12 +7,36 @@
 <title>Signin form</title>
 </head>
 <body>
-<?php include "blocks/header.php";?>
-<?php include "blocks/leftmenu.php";?>
-<?php include "blocks/footer.php";?>
 
 <!-- connect sign in form -->
-<?php include "blocks/auth.php" ?>
+<!-- start session php -->
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+session_start();
+
+if (isset($_POST['login']) && isset($_POST['pass']))
+{
+if ($_POST['login']==='admin' && $_POST['pass']==='pwd') {
+	$_SESSION['auth']='1'
+	header('Location: index.php');
+	exit;
+}
+}
+	?>
+	<div class="formcontainer">
+	<form class="form-signin" role="form" method="POST">
+	<h2 class="form-signin-heading">Sign in form</h2>
+	<input type="login" class="form-control" placeholder="Your login" required="" autofocus="" name="login" value="">
+	<input type="password" class="form-control" placeholder="Password" required="" name="pass" value="">
+	<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+	<a class="form-signin right" href="index.php?logout">Go back</a>
+	</form>
+	</div>
+	<?php
+	die;
+}
+?>
 
 <!-- connect script files -->
 <?php include "blocks/script.php" ?>
