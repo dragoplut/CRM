7c2b850bba120e6d7e977d5a2dd28986659651c7
dragoplut <oleksandr_p@i.ua>
2015-09-28 10:21:42 +0300

    commit

--- views/board.php
+++
@@ -1,9 +0,0 @@
-<?php include "header.php"?>
-    <div align="center">
-        <p>"Let your life to bloom!"</p>
-        <a href="/logout/index" type="submit" name="logout">Log out</a>
-    </div>
-    <div align="center">
-        <a href="/flowers/itemlist" type="submit" name="logout">Flowers list</a>
-    </div>
-<?php include "footer.php"?>
