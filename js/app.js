32b4614bcd241f4cdcec9796f6b2683a424bba73
dragoplut <oleksandr_p@i.ua>
2015-10-15 18:36:51 +0300

    pagination

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 var characters = [];
+var numElem = 6;
+var numPages = '';
+var page = 1;
 
 $(function(){
     loadCharacters(charactersLoaded)
 })
+
+function charactersLoaded(data){
+    characters = data;
+    console.log(characters);
+    $("#charactersTemp").html(renderHTML(characters));
+}
 
 function loadCharacters(callback){
     $.ajax({
@@ -13,17 +22,30 @@ function loadCharacters(callback){
     })
 }
 
-function charactersLoaded(data){
-    characters = data;
-    console.log(characters);
+function noSort(){
     $("#charactersTemp").html(renderHTML(characters));
+}
+
+function pagination(pageNum){
+    page = pageNum;
+    noSort();
 }
 
 function renderHTML(characters){
     var blocks = [];
-    for (var i = 0; i < characters.results.length; i++){
-        var template = '<div class="characterbox" id="' + characters.results[i].id + '" name="tempDiv' + i + '"><img src="' + characters.results[i].thumbnail.path + '.' + characters.results[i].thumbnail.extension + '" align="left" class="demoimg" alt="' + characters.results[i].name + '"><h5 class="center">' + characters.results[i].name + '</h5><a href="#" align="center">Видалити</a></div>';
+    for (var i = (numElem *(page - 1)); i < (numElem * page); i++){
+        if (i < characters.results.length) {
+            var template = '<div class="characterbox" id="' + characters.results[i].id + '" name="tempDiv' + i + '"><img src="' + characters.results[i].thumbnail.path + '.' + characters.results[i].thumbnail.extension + '" align="left" class="demoimg" alt="' + characters.results[i].name + '"><h5 class="center">' + characters.results[i].name + '</h5><h6 align="center"><a href="#" shape="rect">Видалити</a></h6></div>';
+        }
         blocks += template;
+        template = '';
     }
+    numPages = Math.ceil(characters.results.length / numElem);
+    var pageControls = '<div class="topMenu" id="pagingControls">';
+    for (var j = 1; j <= numPages; j++){
+        pageControls += '<div class="btn-group"><button type="button" class="btn-sm btn-info" onclick="pagination(' + j + ')">' + j + '</button></div>';
+    }
+    pageControls += '</div>';
+    $("#pagingControls").html(pageControls);
     return blocks;
 }
