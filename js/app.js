1b557c7a7719d5c7abd529e034847a4e665a2853
dragoplut <oleksandr_p@i.ua>
2015-10-24 02:34:37 +0300

    url pagination

--- js/app.js
+++ js/app.js
@@ -2,10 +2,25 @@ var characters = [];
 var numElem = 6;
 var numPages = '';
 var page = 1;
+var pageValue;
 
 $(function(){
-    loadCharacters(charactersLoaded)
-})
+    loadCharacters(charactersLoaded);
+    parse_url();
+    if (pageValue != undefined){
+        pagination(pageValue);
+    }
+    else{
+        pagination(page)
+    }
+});
+
+$(document).ready(function(){
+    $("#btn-slide").click(function(){
+        $("#panel").slideToggle("slow");
+        $(this).toggleClass("active");
+    })
+});
 
 function charactersLoaded(data){
     characters = data;
@@ -28,14 +43,25 @@ function noSort(){
 
 function pagination(pageNum){
     page = pageNum;
+    var urlPart = "/index.html?page=" + page;
+    history.pushState(page, "MARVEL", urlPart);
     noSort();
+}
+
+function parse_url(){
+    var parser = /page=([^&]+)/i;
+    if (!!parser.exec(document.location.search)){
+        pageValue = parser.exec(document.location.search)[1];
+    }
+    console.log(pageValue);
+    return pageValue;
 }
 
 function renderHTML(characters){
     var blocks = [];
     for (var i = (numElem *(page - 1)); i < (numElem * page); i++){
         if (i < characters.results.length) {
-            var template = '<div class="characterbox" id="' + characters.results[i].id + '" name="tempDiv' + i + '"><img src="' + characters.results[i].thumbnail.path + '.' + characters.results[i].thumbnail.extension + '" align="left" class="demoimg" alt="' + characters.results[i].name + '"><h5 class="center">' + characters.results[i].name + '</h5><h6 align="center"><a href="#" shape="rect">Видалити</a></h6></div>';
+            var template = '<div class="characterbox" id="' + characters.results[i].id + '" name="tempDiv' + i + '"><img src="' + characters.results[i].thumbnail.path + '.' + characters.results[i].thumbnail.extension + '" align="left" class="demoimg img-rounded" alt="' + characters.results[i].name + '"><h5 class="center">' + characters.results[i].name + '</h5><h6 align="center"><a href="#" shape="rect">Видалити</a></h6></div>';
         }
         blocks += template;
         template = '';
@@ -47,5 +73,15 @@ function renderHTML(characters){
     }
     pageControls += '</div>';
     $("#pagingControls").html(pageControls);
+    var HeroList = '';
+    for (var l = 0; l < characters.results.length; l++){
+        HeroList += '<li id="dropdownHero"><a href="#">' + characters.results[l].name + '</a></li>';
+    }
+    $("#dropdownHeroList").html(HeroList);
     return blocks;
+}
+
+function slideForm (){
+    $("#panel").slideToggle("slow");
+    $(this).toggleClass("active");
 }
