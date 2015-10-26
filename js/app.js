76b26bba79fb31d790f4409481b979ba9fe8fd1e
dragoplut <oleksandr_p@i.ua>
2015-10-26 17:51:29 +0200

    client details works

--- js/app.js
+++ js/app.js
@@ -25,10 +25,9 @@ function loadClients(callback){
 
 function renderDetails(clientId){
     var clientRendered = '';
-    debugger;
     for (var i = 0; i < clients.length; i++){
         if (clients[i].id === clientId){
-            clientRendered = '<div class="container"><img class="bigImg" src="' + clients[i].image + '"><h4>' + clients[i].lastName + ' ' + clients[i].firstName + '</h4><br><br><h3>Телефон: ' + clients[i].phone + '</h3><br><h3>Е-мейл: ' + clients[i].email + '</h3><br><h3>Адресса: ' + clients[i].address + '</h3><br><br><h3>Коротко про себе: ' + clients[i].description + '</h3></div>';
+            clientRendered = '<div class="container fixMaxWith"><img class="bigImg modal-content" src="' + clients[i].image + '"><h4 class="text-center">' + clients[i].lastName + ' ' + clients[i].firstName + '</h4><h5>Телефон: ' + clients[i].phone + '</h5><h5>Е-мейл: ' + clients[i].email + '</h5><h5>Адресса: ' + clients[i].address + '</h5><br><p>Коротко про себе: ' + clients[i].description + '</p></div>';
         }
     }
     $("#clientInfo").html(clientRendered);
@@ -37,7 +36,7 @@ function renderDetails(clientId){
 function renderHTML(clients){
     var blocks = [];
     for (var i = 0; i < clients.length; i++){
-        var template = '<tr class="success" id="' + clients[i].id + '"><td><a href="#editClient" data-toggle="modal">Редагувати</a><br><a href="#" onclick="renderDetails(' + clients[i].id + ')">Видалити</a><br><a href="#detailsClient" data-toggle="modal">Детальніше</a></td><td><img src="' + clients[i].image + '"></td><td>' + clients[i].lastName + '</td><td>' + clients[i].firstName + '</td><td>' + clients[i].phone + '</td><td>' + clients[i].email + '</td><td>' + clients[i].address + '</td><td>' + clients[i].description + '</td></tr>';
+        var template = '<tr class="success" id="' + clients[i].id + '"><td><a href="#editClient" data-toggle="modal">Редагувати</a><br><a href="#">Видалити</a><br><a href="#detailsClient" data-toggle="modal" onclick="renderDetails(\'' + clients[i].id + '\')">Детальніше</a></td><td><img class="demoImg" src="' + clients[i].image + '"></td><td>' + clients[i].lastName + '</td><td>' + clients[i].firstName + '</td><td>' + clients[i].phone + '</td><td>' + clients[i].email + '</td><td>' + clients[i].address + '</td><td>' + clients[i].description + '</td></tr>';
         blocks += template;
     }
     return blocks;
