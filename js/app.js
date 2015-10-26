af11290737f27cb0193ba09578bbb3ae17e4f44a
dragoplut <oleksandr_p@i.ua>
2015-10-26 14:30:30 +0200

    clients table and empty popup dialog

--- js/app.js
+++ js/app.js
@@ -4,8 +4,14 @@ $(function(){
     loadClients(clientsLoaded);
 });
 
+
+function clientsBase(){
+    console.log(clients);
+}
+
 function clientsLoaded(data){
     clients = data;
+    $("#blankTr").html(renderHTML(clients));
 }
 
 function loadClients(callback){
@@ -17,6 +23,15 @@ function loadClients(callback){
     })
 }
 
-function clientsBase(){
-    console.log(clients);
+function renderHTML(clients){
+    var blocks = [];
+    for (var i = 0; i < clients.length; i++){
+        var template = '<tr class="success" id="' + clients[i].id + '"><td><a href="#editClient" data-toggle="modal">Редагувати</a><br><a href="#">Видалити</a><br><a href="#detailsClient" data-toggle="modal">Детальніше</a></td><td><img src="' + clients[i].image + '"></td><td>' + clients[i].lastName + '</td><td>' + clients[i].firstName + '</td><td>' + clients[i].phone + '</td><td>' + clients[i].email + '</td><td>' + clients[i].address + '</td><td>' + clients[i].description + '</td></tr>';
+        blocks += template;
+    }
+    return blocks;
+}
+
+function renderTable(){
+    $("#blankTr").html(renderHTML(clients));
 }
