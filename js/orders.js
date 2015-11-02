a0b2738ec11cb40e389ca8fc179d2fa1a38cdf93
dragoplut <oleksandr_p@i.ua>
2015-11-02 13:56:07 +0200

    render table without pagination, delete order works, misunderstandings with client id in saveOrder

--- js/orders.js
+++ js/orders.js
@@ -2,7 +2,17 @@ var allOrders = [];
 var clientsFullList = [];
 var existClientId = '';
 var searchOptionOrders = '';
-var statusOption = 'Нове';
+var statusOption = 'new';
+
+$(function(){
+    ordersBase();
+});
+
+$("#searchRequestOrders").keypress(function(event){
+    if(event.keyCode == 13) {
+        searchOrder();
+    }
+});
 
 function clearFormOrder(){
     $("#inputSearchExistClient").val('');
@@ -10,6 +20,27 @@ function clearFormOrder(){
     $("#price").val('');
     $("#payDate").val('');
     $("#descriptionOrders").val('');
+}
+
+function deleteOrder(orderId){
+    if (confirm('Ви впевнені що хочете видалити це замовлення?')){
+        var id = {id: orderId};
+        var elem = document.getElementById(orderId);
+        elem.remove();
+        $.ajax({
+            type: 'DELETE',
+            url: 'http://apishop.herokuapp.com/order',
+            data: id,
+            success: function(result){
+                allOrders = result;
+                alert("Видалено замовлення id: " + orderId);
+            }
+        })
+    }
+}
+
+function editOrder(){
+    console.log('Редагувати замовлення');
 }
 
 function newOrder(){
@@ -41,22 +72,45 @@ function newOrderSave(){
 }
 
 function ordersBase(){
-    console.log(allOrders);
+    var urlRequest = 'http://apishop.herokuapp.com/order';
+    $.ajax({
+        url: urlRequest,
+        success: function(result){
+            ordersLoaded(result);
+        }
+    });
 }
 
-function renderOrdersTable(){
+function ordersLoaded(data){
+    allOrders = data;
+    $("#blankTrOrders").html(renderOrdersTable(data));
+}
 
+function renderDetailsOrder(){
+    console.log('Деталі замовлення');
+}
+
+function renderOrdersTable(data){
+    allOrders = data;
+    console.log(allOrders);
+    var blocks = [];
+    for (var i = 0; i < allOrders.length; i++){
+        var template = '<tr class="success" id="' + allOrders[i].id + '"><td class="text-center"><a title="Детальніше" href="#orderDetailsForm" data-toggle="modal" onclick="renderDetailsOrder(\'' + allOrders[i].id + '\')"><span class="glyphicon glyphicon-file"></span></a><br><a title="Редагувати" href="#editOrder" data-toggle="modal" onclick="editOrder(\'' + allOrders[i].id + '\')"><span class="glyphicon glyphicon-pencil"></span></a><br><a title="Видалити" href="#" onclick="deleteOrder(\'' + allOrders[i].id + '\')"><span class="glyphicon glyphicon-trash"></span></a></td><td>' + allOrders[i].title + '</td><td datatype="number">' + allOrders[i].price + '</td><td datatype="date">' + allOrders[i].payDate + '</td><td>' + allOrders[i].status + '</td><td>' + clients[i].lastName + ' ' + clients[i].firstName + '</td></tr>';
+        blocks += template;
+    }
+    var sortClientLnFn = 'Клієнт<a class="pagingButtons" title="За зростанням" onclick=""><span class="glyphicon glyphicon-chevron-down"></span></a><a class="pagingButtons" title="За спаданням" onclick=""><span class="glyphicon glyphicon-chevron-up"></span></a>';
+    var sortFirst = 'Ім’я<a class="pagingButtons" title="За зростанням" onclick="sortDirect=fnASC ,pagination()"><span class="glyphicon glyphicon-chevron-down"></span></a><a class="pagingButtons" title="За спаданням" onclick="sortDirect=fnDESC,pagination()"><span class="glyphicon glyphicon-chevron-up"></span></a>';
+    var pagingControlsOrders = '<a class="pagingButtons" title="Попередня сторінка" onclick="controlsFwdBwd(0)"><span class="glyphicon glyphicon-backward"></span></a><a class="pagingButtons">' + pageValue + '</a><a class="pagingButtons" title="Наступна сторінка" onclick="controlsFwdBwd(1)"><span class="glyphicon glyphicon-forward"></span></a>';
+    $("#sortClientLnFn").html(sortClientLnFn);
+    $("#").html();
+    $("#pagingControlsOrdersTop").html(pagingControlsOrders);
+    $("#pagingControlsOrdersBot").html(pagingControlsOrders);
+    return blocks;
 }
 
 function searchOrder(){
 
 }
-
-$("#searchRequestOrders").keypress(function(event){
-    if(event.keyCode == 13) {
-        searchOrder();
-    }
-});
 
 function searchExistClient(){
     var searchRequest = document.getElementById('inputSearchExistClient').value;
@@ -81,6 +135,7 @@ function showClientId(){
 }
 
 function updateRESTnewOrder(addOrder){
+    console.log(existClientId);
     console.log(addOrder);
     $.ajax({
         url: 'http://apishop.herokuapp.com/order',
@@ -88,7 +143,6 @@ function updateRESTnewOrder(addOrder){
         data: addOrder,
         success: function(result){
             allOrders = result;
-            paginationOrder();
             alert('Нове замовлення успішно створено і додано в базу.');
         },
         error: function(){
